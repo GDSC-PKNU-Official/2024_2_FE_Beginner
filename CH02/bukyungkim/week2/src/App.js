cb4b674fdafa47d63535797c58ff7b3ce68c9ff9
Kimbukyung <qnrud8637@gmail.com>
2024-11-24 21:09:53 +0900

    commit message

--- CH02/bukyungkim/week2/src/App.js
+++ CH02/bukyungkim/week2/src/App.js
@@ -1,8 +1,9 @@
-import { useState, useRef } from "react";
 import "./App.css";
 import Header from "./component/Header";
 import TodoEditor from "./component/TodoEditor";
 import TodoList from "./component/TodoList";
+import { useState, useRef } from "react";
+
 const mockTodo = [ 
   {
     id: 0,
@@ -24,14 +25,13 @@ const mockTodo = [
   },
 ];
 
-
 function App() {
   const idRef = useRef(3);
   const [todo, setTodo] = useState(mockTodo);
 
-  const onCreate = (content) => { 
+  const onCreate = (content) => {
     const newItem = {
-      id: idRef.current,
+      id: idRef.current, 
       content,
       isDone: false,
       createdDate: new Date().getTime(),
@@ -39,13 +39,23 @@ function App() {
     setTodo([newItem, ...todo]);
     idRef.current += 1; 
   };
-  
-  return (
-    <div className="App">
-      <Header />
-      <TodoEditor onCreate={onCreate} />
-      <TodoList todo={todo} />
-    </div>
-  );
+  const onUpdate = (targetId) => {
+    setTodo(
+      todo.map((it) =>
+        it.id === targetId ? { ...it, isDone: !it.isDone } : it
+      )
+    );
+  };
+  const onDelete = (targetId) => { 
+    setTodo(todo.filter((it) => it.id !== targetId));
+  };
+
+return (
+  <div className="App">
+    <Header />
+    <TodoEditor onCreate={onCreate} />
+    <TodoList todo={todo} onUpdate={onUpdate} onDelete={onDelete} /> 
+  </div>
+);
 }
-export default App;
+export default App
